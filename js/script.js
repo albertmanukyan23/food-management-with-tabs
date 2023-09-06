a255092de8f32fc5a9f11077fa17d28e291b5166
Albert Manukyan <albertmanukyan975@gmail.com>
2023-09-07 00:26:23 +0400

    "add timer to the project"

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+    //TABS
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabsContent = document.querySelectorAll('.tabcontent');
     const tabsParent = document.querySelector('.tabheader__items');
@@ -36,4 +36,45 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
+
+    //TIMER 
+    const deadline = '2023-09-07';
+    function getTimeRemaining(endTime) {
+        const delta = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor(delta / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((delta / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((delta / 1000 * 60) % 60),
+            seconds = Math.floor((delta / 1000) % 60);
+        return {
+            'total': delta,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInteryal = setInterval(updateCloak, 1000);
+
+        updateCloak();
+
+        function updateCloak() {
+            const temp = getTimeRemaining(endtime);
+            days.innerHTML = temp.days;
+            hours.innerHTML = temp.hours;
+            minutes.innerHTML = temp.minutes;
+            seconds.innerHTML = temp.seconds;
+            if (temp.total <= 0) {
+                clearInterval(timeInteryal);
+            }
+
+        }
+    }
+    setClock('.timer', deadline);
 })
